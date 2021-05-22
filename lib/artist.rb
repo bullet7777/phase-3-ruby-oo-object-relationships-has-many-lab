d912e88316c9e72b86151c27005196ddedcc700d
chris <chris.mahfouz@hotmail.com>
2021-05-22 20:33:30 +0300

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,47 @@
+require "pry"
+
+class Artist
+    attr_accessor :name ,:songs
+
+
+
+    def initialize(name)
+        @name=name
+       # @songs=[]
+       
+  
+        
+
+    end
+    
+
+    def songs
+        Song.all.select do |song| 
+         
+            self == song.artist
+
+
+        end
+      
+      
+    end
+    def add_song(song)
+        #@songs << song
+        songs
+        song.artist= self
+        
+
+    end
+    def add_song_by_name(name)
+        song=Song.new(name)
+        add_song(song)
+
+    end
+    def self.song_count
+        Song.all.length
+    end
+
+    
+
+
+end
